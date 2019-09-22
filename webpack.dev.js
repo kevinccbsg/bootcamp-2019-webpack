65f3195aab161e3d4530377edb23a82971e9560d
kevin Martínez <kevinccbsg@gmail.com>
2019-09-22 19:14:59 +0200

    Handling basic css

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,17 @@ const { join } = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: join(__dirname, 'webpack-examples', 'basic.js'),
+  entry: join(__dirname, 'webpack-examples', 'handling-css.js'),
   output: {
     path: join(__dirname, 'build'),
     filename: 'bundle.[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
   },
 };
