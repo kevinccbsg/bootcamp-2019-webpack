35fe8e816ce8447370ae50813fe6270d688d72b1
kevin Martínez <kevinccbsg@gmail.com>
2019-09-22 19:26:41 +0200

    Testing css modules

--- webpack-examples/handling-css.js
+++ webpack-examples/handling-css.js
@@ -1,3 +1,13 @@
-import './css/styles.css';
+import styles from './css/styles.css';
 
 console.log('Handling CSS');
+
+const template = () => `
+  <div class="${styles.myExample}">Lorem ipsum dolor</div>
+`;
+
+const renderSomething = (template, element) => {
+  element.innerHTML = template;
+};
+
+renderSomething(template(), document.body);
